231c62d641df0bf656f4423c654fa018c17dcc93
myun2 <myun2@nwhite.info>
2015-03-09 16:25:11 +0900

    init

---
+++ driver.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "util.hpp"
+
+int main()
+{
+	cls();
+	return 0;
+}
---
+++ util.hpp
@@ -0,0 +1,8 @@
+#ifndef __MYUN2__DRIVER__UTIL_HPP__
+#define __MYUN2__DRIVER__UTIL_HPP__
+
+#include <stdio.h>
+
+#define cls() printf("\e[1;1H\e[2J")
+
+#endif//__MYUN2__DRIVER__UTIL_HPP__
